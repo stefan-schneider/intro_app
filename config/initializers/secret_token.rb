060f000086a36a568d3a4b4e7cd358ec63783c43
Stefan Schneider <schneidito@gmail.com>
2013-09-16 00:59:00 +0200

    Initialise repo, ok for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntroApp::Application.config.secret_key_base = 'a2ecab02fcdba8c6019760da20255e28ad74c918c5257f7f4435d3b01debfd4570cd6c6d6bc4ed1b71b2a0cf36111f93d40f119d2e92d97a448aa772525ac74c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IntroApp::Application.config.session_store :cookie_store, key: '_intro_app_session'
